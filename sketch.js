238533d9006839f20e4e16036a449db47352941e
Tanishkagaba2008 <84225116+Tanishkagaba2008@users.noreply.github.com>
2021-06-05 13:58:25 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+
+var database,position;
+var playerCount,gameState=0;
+var form,player,game;
+
+
+function setup(){
+    createCanvas(500,500);
+    database=firebase.database();
+    game= new Game()
+    game.getState()
+    game.start()
+
+    
+}
+
+function draw(){}
+
